2c6f5d98a56dc57f1b7f64b4f6650890f3e4d3f7
TheFabi8A <cafaruoc2000@hotmail.com>
2023-04-14 21:18:46 -0500

    challengue concluded

---
+++ encripter.js
@@ -0,0 +1,91 @@
+const textUser = document.getElementById('textarea-user');
+const textResult = document.getElementById('textarea-result');
+const btnCript = document.getElementById('btn-cript');
+const btnUncript = document.getElementById('btn-uncript');
+const btnCopy = document.getElementById('btn-copy');
+const msgError = document.getElementById('message-error');
+const textEmpty = document.getElementById('text-empty-alert');
+const containerResult = document.getElementById('container-result');
+
+const criptWords = {
+  'a': 'ai',
+  'e': 'enter',
+  'i': 'imes',
+  'o': 'ober',
+  'u': 'ufat',
+};
+
+const uncriptWords = {
+  'ai': 'a',
+  'enter': 'e',
+  'imes': 'i',
+  'ober': 'o',
+  'ufat': 'u',
+};
+
+btnCopy.addEventListener('click', () => {
+  const text = textResult.value;
+  navigator.clipboard.writeText(text);
+});
+
+btnCript.addEventListener('click', () => {
+  const text = textUser.value.toLowerCase();
+  const criptText = criptOrUncript(text, criptWords);
+
+  if (hasSpecialChars(text)) {
+    msgError.classList.remove('hidden');
+    setTimeout(()=>{
+      msgError.classList.add('hidden');
+    }, 3000);
+  } else {
+    const criptText = criptOrUncript(text, criptWords);
+    textResult.innerHTML = criptText;
+  }
+
+  if (textUser.value.length === 0) {
+    textEmpty.classList.remove('hidden');
+    containerResult.classList.add('hidden');
+    setTimeout(()=>{
+      textEmpty.classList.add('hidden');
+      containerResult.classList.remove('hidden');
+    }, 5000);
+  }
+  textResult.innerHTML = criptText;
+});
+
+btnUncript.addEventListener('click', () => {
+  const text = textUser.value.toLowerCase();
+  const uncriptText = criptOrUncript(text, uncriptWords);
+
+  if (hasSpecialChars(text)) {
+    msgError.classList.remove('hidden');
+    setTimeout(()=>{
+      msgError.classList.add('hidden');
+    }, 3000);
+  } else {
+    const uncriptText = criptOrUncript(text, criptWords);
+    textResult.innerHTML = uncriptText;
+  }
+
+  if (textUser.value.length === 0) {
+    textEmpty.classList.remove('hidden');
+    containerResult.classList.add('hidden');
+    setTimeout(()=>{
+      textEmpty.classList.add('hidden');
+      containerResult.classList.remove('hidden');
+    }, 5000);
+  }
+  textResult.innerHTML = uncriptText;
+});
+
+function hasSpecialChars(text) {
+  const specialChars = /[!@#$%^&*(),.?":{}|<>ñáéíóú]/g;
+  return specialChars.test(text);
+}
+
+function criptOrUncript(text, keys) {
+  const regex = new RegExp(Object.keys(keys).join('|'), 'g');
+  return text.replace(regex, (match) => keys[match]);
+}
+
+
